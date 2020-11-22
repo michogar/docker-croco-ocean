108fe0a0226ff30dee4e6f8801fe6e1c03b7f035
michogar <michogar@gmail.com>
2020-11-22 13:48:22 +0100

    Modifies run script to get name from param

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-PROJECT="Run_caca"
+PROJECT="Run_config"
+
+if [ $1 ]
+then
+    PROJECT=Run_$1
+    echo You will create a config named $PROJECT
+fi
+echo Running Docker...
 docker run --rm -ti -e MY_CONFIG_NAME=$PROJECT -v $(pwd)/$PROJECT:/opt/croco/$PROJECT --name croco micho/croco /bin/bash
